40dfddda73c2c3caf1fb0a1c1ebe33f90f89b7b7
0x26e7d <geaarwhwu@hotmail.com>
2026-02-02 10:53:07 +0800

    chapter3 print function

---
+++ chapter3/include/asm/system.h
@@ -0,0 +1,10 @@
+#ifndef _SYSTEM_H
+
+#define _SYSTEM_H
+
+#define sti() __asm__("sti" ::)
+#define cli() __asm__("cli" ::)
+#define nop() __asm__("nop" ::)
+#define iret() __asm__("iret" ::)
+
+#endif // !_SYSTEM_H
--- chapter3/kernel/chr_drv/console.c
+++ chapter3/kernel/chr_drv/console.c
@@ -1,3 +1,5 @@
+#include <asm/io.h>
+#include <asm/system.h>
 #include <linux/tty.h>
 
 #define ORIG_X (*(unsigned char *)0x90000)
@@ -24,6 +26,135 @@ static unsigned long video_size_row;
 static unsigned char video_page;
 static unsigned short video_port_reg;
 static unsigned short video_port_val;
+static unsigned short video_erase_char;
+
+static unsigned long origin;
+static unsigned long scr_end;
+static unsigned long pos;
+static unsigned long x, y;
+static unsigned long top, bottom;
+static unsigned long attr = 0x07;
+
+static inline void gotoxy(int new_x, unsigned int new_y) {
+  if (new_x > video_num_columns || new_y >= video_num_lines)
+    return;
+
+  x = new_x;
+  y = new_y;
+  pos = origin + y * video_size_row + (x << 1);
+}
+
+static inline void set_origin() {
+  cli();
+  outb_p(12, video_port_reg);
+  outb_p(0xff & ((origin - video_mem_base) >> 9), video_port_val);
+  outb_p(13, video_port_reg);
+  outb_p(0xff & ((origin - video_mem_base) >> 1), video_port_val);
+  sti();
+}
+
+static void scrup() {
+  if (!top && bottom == video_num_lines) {
+    origin += video_size_row;
+    pos += video_size_row;
+    scr_end += video_size_row;
+    if (scr_end > video_mem_term) {
+      __asm__("cld\n\t"
+              "rep\n\t"
+              "movsl\n\t"
+              "movl video_num_columns,%1\n\t"
+              "rep\n\t"
+              "stosw\n\t" ::"a"(video_erase_char),
+              "c"((video_num_lines - 1) * video_num_columns >> 1),
+              "D"(video_mem_base), "S"("origin")
+              :);
+      scr_end = origin - video_mem_base;
+      pos -= origin - video_mem_base;
+      origin = video_mem_base;
+    } else {
+      __asm__("cld\n\t"
+              "rep\n\t"
+              "stosw\n\t" ::"a"(video_erase_char),
+              "c"(video_num_columns), "D"(scr_end - video_size_row)
+              :);
+    }
+    set_origin();
+  } else {
+    __asm__("cld\n\t"
+            "rep\n\t"
+            "movsl\n\t"
+            "movl video_num_columns,%%ecx\n\t"
+            "rep\n\t"
+            "stosw\n\t" ::"a"(video_erase_char),
+            "c"((bottom - top - 1) * video_num_columns >> 1),
+            "D"(origin + video_size_row * top),
+            "S"(origin + video_size_row * (top + 1))
+            :);
+  }
+}
+
+static inline void set_cursor() {
+  cli();
+  outb_p(14, video_port_reg);
+  outb_p(0xff & ((pos - video_mem_base) >> 9), video_port_val);
+  outb_p(15, video_port_reg);
+  outb_p(0xff & ((pos - video_mem_base) >> 1), video_port_val);
+  sti();
+}
+
+static void lf() {
+  if (y + 1 < bottom) {
+    y++;
+    pos += video_size_row;
+    return;
+  }
+  scrup();
+}
+
+static void cr() {
+  pos -= x << 1;
+  x = 0;
+}
+
+static void del() {
+  if (x) {
+    pos -= 2;
+    x--;
+    *(unsigned short *)pos = video_erase_char;
+  }
+}
+
+void console_print(const char *buf, int nr) {
+  char *s = buf;
+
+  while (nr--) {
+    char c = *s++;
+    if (c > 31 && c < 127) {
+      if (x >= video_num_columns) {
+        x -= video_num_columns;
+        pos -= video_size_row;
+        lf();
+      }
+      *(char *)pos = c;
+      *(((char *)pos) + 1) = attr;
+      pos += 2;
+      x++;
+    } else if (c == 10 || c == 11 || c == 12) {
+      lf();
+    } else if (c == 13) {
+      cr();
+    } else if (c == 127) {
+      del();
+    } else if (c == 8) {
+      if (x) {
+        x--;
+        pos -= 2;
+      }
+    }
+    gotoxy(x, y);
+    set_cursor();
+  }
+}
 
 void con_init() {
   char *display_desc = "????";
@@ -52,6 +183,18 @@ void con_init() {
   display_ptr = ((char *)video_mem_base) + video_size_row - 8;
   while (*display_desc) {
     *display_ptr++ = *display_desc++;
-    *display_ptr++;
+    display_ptr++;
+  }
+
+  origin = video_mem_base;
+  scr_end = video_mem_base + video_num_lines * video_size_row;
+  top = 0;
+  bottom = video_num_lines;
+
+  gotoxy(ORIG_X, ORIG_Y);
+  set_cursor();
+  console_print("\r\n", 2);
+  for (int i = 0; i < 20; i++) {
+    console_print("hello\r\n", 7);
   }
 }
